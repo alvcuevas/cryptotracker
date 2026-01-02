0f36d5ec353f0948a0b27d6ad2a6286121aafbb8
Alvaro Cuevas <alvaro.cuevas@asesormasmovil.es>
2026-01-02 16:19:21 +0100

    feat: coin overview & trending coins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.coingecko.com",
+      },
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+      },
+    ],
+  },
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
